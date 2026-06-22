e6d0b780ea467919d859d699404c3ad91c609725
Pkaynan <pietrokaynan1@gmail.com>
2026-06-22 09:33:22 -0300

    first commit

---
+++ src/main/java/com/pietro/forademao/ForademaoApplication.java
@@ -0,0 +1,13 @@
+package com.pietro.forademao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForademaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForademaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pietro/forademao/ForademaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pietro.forademao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForademaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
