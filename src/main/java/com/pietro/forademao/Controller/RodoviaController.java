08dff3df61157db83b472628e60175b9a5cf3455
Pkaynan <pietrokaynan2@gmail.com>
2026-08-14 10:24:33 -0300

    arrumando erros de digitacao

--- src/main/java/com/pietro/forademao/Controller/RodoviaController.java
+++ src/main/java/com/pietro/forademao/Controller/RodoviaController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/clima")
+@RequestMapping("/rodovia")
 public class RodoviaController {
 
     private final RodoviaService rodoviaService;
